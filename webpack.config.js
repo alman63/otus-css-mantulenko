1c92766f54a4d3bc2c20148220ca02c6eac1ed4e
alman63 <mantulenko83@mail.ru>
2022-10-12 11:29:45 +0400

    подготовил файлы, начинаю установку модулей

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/style.css',
+    mode: 'development',
+    output: {
+        filename: 'style.css',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.(css)$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(jpeg|jpg|png)$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.(svg)$/,
+                use: 'svg-inline-loader',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: '../public/index.html',
+        }),
+    ],
+    devServer: {
+        compress: false,
+        open: '/',
+        port: 3001,
+    },
+}
